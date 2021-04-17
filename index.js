d705b068f3e6a856734dbeddb0a12ee43ae17b9a
jakob <jakob.germano@gmail.com>
2021-04-16 18:05:00 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+const { arrayContaining } = require("expect")
+
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers, name) {
+   return drivers.filter(function(driver){
+        return driver.toUpperCase() === name.toUpperCase()
+    })
+}
+
+function fuzzyMatch(drivers, members)  {
+    return drivers.filter(el => el.toLowerCase().indexOf(members.toLowerCase()) == 0)
+}
+
+function matchName(drivers, names) {
+    return drivers.filter(function(driver) {
+    return driver.name == names });
+}
+
